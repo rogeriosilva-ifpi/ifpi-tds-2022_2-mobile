a6bb8f4872790283a532f84687c043fc442ee2ad
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2022-08-30 21:53:04 -0300

    BMI App started

---
+++ aplicativos/bmiapp/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(title: 'BMI App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 60),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'BMI Calculator',
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Digite seu nome',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Digite sua altura(m)',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Digite seu peso(kg)',
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('CALCULAR'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
